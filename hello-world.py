1a9b027aaafebb92acb50e1a3753ef44428e6014
Mateusz <smetmateusz@gmail.com>
2022-12-05 12:43:21 +0100

    Added randomness
    Added randomness to hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python3
+
+import random as rand
+
 print("Printing Hello World!")
 
 print("Adding some numbers", 12 + 12)
+print("Adding new random numbers", rand.randrange(1,20) + rand.randrange(1,20))
 
