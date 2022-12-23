18edd177a34d2ba8b5295826fe53425719fb3b56
Mateusz <smetmateusz@gmail.com>
2022-12-23 10:39:44 +0100

    Corrected spelling

--- hello-world.py
+++ hello-world.py
@@ -4,9 +4,9 @@ import random as rand
 
 print("Printing Hello World!")
 
-# Adding some numbers together just for fun
+# Adding some numbers together just for fun :) 
 print("Adding some numbers", rand.randrange(1, 50) +  rand.randrange(1, 50)) 
 
-# Adding a new random numbers, this time it's not for fun
+# Adding new set of random numbers, this time it's not for fun :/ 
 print("Adding new random numbers", rand.randrange(1,20) + rand.randrange(1,20))
 
